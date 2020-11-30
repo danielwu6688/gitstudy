1b428731991c80963d5857dc1915e9e0c611fd30
daniel <danielwu8866@gmail.com>
2020-11-30 23:09:45 +0800

    第二次提交,写了HelloWord

--- src/main/java/com/daniel/gitxuexi/Hello.java
+++ src/main/java/com/daniel/gitxuexi/Hello.java
@@ -3,5 +3,6 @@ package com.daniel.gitxuexi;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("HelloWord");
+        System.out.println("HELLO");
     }
 }
